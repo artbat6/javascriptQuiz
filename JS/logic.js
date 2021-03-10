2047874bad0ea1555770b26ecc9daef9a6424ec2
artbat6 <arthurbatesdev@gmail.com>
2021-03-10 16:04:19 -0600

    got timer working

--- JS/logic.js
+++ JS/logic.js
@@ -1,6 +1,6 @@
 //variables to keep track of quiz state
 var currentQuestionIndex = 0;
-var time = questions.length * 15;
+var time = questions.length * 1;
 var timerId;
 
 // variables to reference DOM elements
@@ -12,41 +12,42 @@ var startBtn = document.getElementById("start");
 var initialsEl = document.getElementById("initials");
 var feedbackEl = document.getElementById("feedback");
 
-startBtn.addEventListener("click", function() {
-    startQuiz();
-    console.log("quiz starting");
+startBtn.addEventListener("click", function () {
+  startQuiz();
+  console.log("quiz starting");
 });
 
 function startQuiz() {
-    //hide start screen
-    var startScreenEl = document.getElementById("start-screen");
-    startScreenEl.setAttribute("class", "hide");
-    //reveal questions section
-    questionsEl.removeAttribute("class");
-    //start timer
-    timerId = setInterval(clockTick, 1000);
-    //show starting time
-    timerEl.textContent = time;
+  //hide start screen
+  var startScreenEl = document.getElementById("start-screen");
+  startScreenEl.setAttribute("class", "hide");
+  //reveal questions section
+  questionsEl.removeAttribute("class");
+  //start timer
+  timerId = setInterval(clockTick, 1000);
+  //show starting time
+  timerEl.textContent = time;
 
-    getQuestion();
+  getQuestion();
 }
 
-function getQuestion(){
-    var currentQuestion = questions[];
-    var titleEl = document.getElementById("question-title");
-    titleEl.textContent = currentQuestion.title;
-    choicesEl.textContent = "";
-    currentQuestion.choices.forEach(function(choice, i){
-        var c = document.createElement("button");
-        c.setAttribute('class', 'choice')
-    });
+function getQuestion() {
+  var currentQuestion = questions[1];
+  var titleEl = document.getElementById("question-title");
+  titleEl.textContent = currentQuestion.title;
+  choicesEl.textContent = "";
+  currentQuestion.choices.forEach(function (choice, i) {
+    var c = document.createElement("button");
+    c.setAttribute("class", "choice");
+  });
 }
 
 function clockTick() {
-    time--;
-    timerEl.textContent = time;
-    if(time === 0) {
-        clearInterval(timerEl);
-        console.log("timer stop")
-    }
+  time--;
+  timerEl.textContent = time;
+  
+  if (time <= 0) {
+    clearInterval(timerId);
+    console.log("timer stop");
+  }
 }
