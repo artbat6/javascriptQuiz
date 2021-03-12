0acd70fea51b5db5a109ee52a84bb540a011c39d
artbat6 <arthurbatesdev@gmail.com>
2021-03-11 18:58:27 -0600

    finished main quiz logic

--- JS/logic.js
+++ JS/logic.js
@@ -1,6 +1,6 @@
 //variables to keep track of quiz state
 var currentQuestionIndex = 0;
-var time = questions.length * 1;
+var time = questions.length * 10;
 var timerId;
 
 // variables to reference DOM elements
@@ -12,10 +12,11 @@ var startBtn = document.getElementById("start");
 var initialsEl = document.getElementById("initials");
 var feedbackEl = document.getElementById("feedback");
 
+
 startBtn.addEventListener("click", function () {
-  startQuiz();
-  console.log("quiz starting");
-});
+    startQuiz();
+  });
+
 
 function startQuiz() {
   //hide start screen
@@ -31,23 +32,67 @@ function startQuiz() {
   getQuestion();
 }
 
+// display question and 4 choices as buttons - iterate through the five questions
+
+function choiceClick() {
+  var answerChoice = this.value;
+  if (answerChoice !== questions[currentQuestionIndex].answer) {
+    time -= 15;
+    if (time < 0) time = 0;
+    timerEl.textContent = time;
+  }
+  currentQuestionIndex++;
+  if (currentQuestionIndex === questions.length) {
+    quizEnd()}
+  else {
+    getQuestion();
+  }
+}
+
 function getQuestion() {
-  var currentQuestion = questions[1];
+  var currentQuestion = questions[currentQuestionIndex];
   var titleEl = document.getElementById("question-title");
   titleEl.textContent = currentQuestion.title;
+  // lost here
   choicesEl.textContent = "";
   currentQuestion.choices.forEach(function (choice, i) {
     var c = document.createElement("button");
-    c.setAttribute("class", "choice");
+    c.setAttribute("value", choice);
+    c.textContent = choice;
+    c.onclick = choiceClick;
+    choicesEl.append(c);
   });
+}
+
+
+
+function quizEnd() {
+    clearInterval(timerId);
+    var endEl = document.getElementById("end-screen");
+    endEl.removeAttribute("class");
+
+    var finalScoreEl = document.getElementById("final-score");
+    finalScoreEl.textContent = time;
+    questionsEl.setAttribute("class", "hide");
+
 }
 
 function clockTick() {
   time--;
   timerEl.textContent = time;
-  
   if (time <= 0) {
-    clearInterval(timerId);
-    console.log("timer stop");
+    quizEnd();
   }
 }
+
+submitBtn.addEventListener("click", function () {
+  saveHighScore();
+});
+;
+
+function saveHighScore() {
+  
+}
+
+
+
