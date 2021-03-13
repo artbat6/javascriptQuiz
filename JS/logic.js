f67523040db3320bec1d82d2309bd8a3273641fe
artbat6 <arthurbatesdev@gmail.com>
2021-03-12 19:18:27 -0600

    appending score list

--- JS/logic.js
+++ JS/logic.js
@@ -11,6 +11,7 @@ var submitBtn = document.getElementById("submit");
 var startBtn = document.getElementById("start");
 var initialsEl = document.getElementById("initials");
 var feedbackEl = document.getElementById("feedback");
+var scoresEl = document.getElementById("hiScoreLink")
 
 startBtn.addEventListener("click", function () {
   startQuiz();
@@ -84,14 +85,58 @@ submitBtn.addEventListener("click", function () {
   saveHighScore();
 });
 
+//_______________________________________________//
+
+var scoreArray = [];
+
+if (JSON.parse(localStorage.getItem("quizData")) !== null) {
+  scoreArray = JSON.parse(localStorage.getItem("quizData"));
+} 
+
 function saveHighScore() {
   timerEl.textContent = time;
   var scoreInfo = {
     initials: initialsEl.value,
     score: time,
   };
-  localStorage.setItem("quiz data", JSON.stringify(scoreInfo));
-  
-  var scoreList = document.createElement("li")
+  scoreArray.push(scoreInfo);
+  localStorage.setItem("quizData", JSON.stringify(scoreArray));
+}
+
+
+function viewHighScores() {
+  for (i = 0; i < scoreArray.length; i++){
+    console.log("this is i");
+    var listItem = document.createElement("li");
+    //create a list item
+    listItem.value = scoreArray[i].initials + " " + scoreArray[i].score;
+    
+  //give the list item a value (from score array)
+    var listContainer = document.getElementById("score-list");
+    //append li to ul
+    listContainer.appendChild(listItem);
+  }
 
 }
+
+
+scoresEl.addEventListener("click", function () {
+  console.log("this is being called");
+  viewHighScores();
+  
+});
+//wrap quiz section in a div
+//give that div an id
+//wrap high score section in div
+//give that div a different id
+//in css reference high score section give it style display none
+//we already have an event listener on the quiz section button
+//when that button is clicked we are going to reference the high score section
+//and give it a style of display block
+//reference the quiz section and give it a style of display none
+//repeat the process with opposite styles with return to quiz button
+
+
+
+
+
